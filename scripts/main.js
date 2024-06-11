f601a42714179640dbe14100657d173944c53898
José Campos <130111367+suportecnico@users.noreply.github.com>
2024-06-11 13:10:17 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Olá Mundo";
+
+let minhaVariavel = "Bebeto";
+// Variável tipo String
+let myNumber = 28;
+// Variável tipo número
+let myVariables = [1, 'bob', 'Steve', 10];
+// Variável tipo Array
+
+let meuNome = document.querySelector("p");
+meuNome.textContent = minhaVariavel + myNumber;
+
